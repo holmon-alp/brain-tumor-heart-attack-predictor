ac0554f7800d2e78c4fe39b4f66680f8ac0b08de
Mirjamol Mirislomov <alpholmon@gmail.com>
2024-02-18 23:19:41 +0500

    Live app

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ st.markdown(
 
     ***
 
-   [Source code of this app](https://github.com/holmon-alp/brain-tumor-heart-attack-predictor.git) 
+   [Source code of this app](https://github.com/holmon-alp/brain-tumor-heart-attack-predictor.git)
+   
    ---
    ##### Created at 18.02.2023
    #### Created by **Mirjamol Mirislomov**
